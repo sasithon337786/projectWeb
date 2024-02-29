c3c2fe7b33e6a93ffdede285ec3928685d1d8ba6
sasithon337786 <65011212034@msu.ac.th>
2024-03-01 00:32:48 +0700

    first commit

---
+++ src/app/nav/navtop10/navtop10.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+@Component({
+  selector: 'app-navtop10',
+  standalone: true,
+  imports: [MatToolbarModule],
+  templateUrl: './navtop10.component.html',
+  styleUrl: './navtop10.component.scss'
+})
+export class Navtop10Component {
+
+}
