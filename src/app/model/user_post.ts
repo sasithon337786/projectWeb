3b3cceba3ad79722c78c58ee487f6048cc8b18f7
sasithon337786 <65011212034@msu.ac.th>
2024-03-01 21:10:49 +0700

    เพิ่มmodel

---
+++ src/app/model/user_post.ts
@@ -0,0 +1,11 @@
+export interface UserPostResponse {
+    user_id:         number;
+    user_email:      string;
+    user_pass:       string;
+    user_type:       number;
+    user_pictrue:    string;
+    user_name:       string;
+    user_age:        null;
+    user_gender:     null;
+    user_preference: null;
+}
