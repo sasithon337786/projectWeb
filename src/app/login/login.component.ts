9efe872912f7351f10400f390f96fc5d6adf7276
sasithon337786 <161368508+sasithon337786@users.noreply.github.com>
2024-03-01 21:06:57 +0700

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,13 +2,30 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { UserGetResponse } from '../model/user_get';
+import { lastValueFrom } from 'rxjs';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatInputModule,MatFormFieldModule,MatButtonModule],
+  imports: [MatInputModule, MatFormFieldModule, MatButtonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
+  constructor(private http: HttpClient) {}
+  user: UserGetResponse[] = [];
+  
+  async findName(name: HTMLInputElement) {
+    const url = 'http://localhost:3000/user';
+    const data = await lastValueFrom(
+      this.http.get(url, {
+        params: {
+          name: name.value,
+        },
+      })
+    );
+    this.user = data as UserGetResponse[];
+    console.log(this.user);
+  }
 }
